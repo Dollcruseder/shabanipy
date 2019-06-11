5e218303d40af42bac87c2ddd8a9ed258e71fc44
Dollcrusader <sy2297@nyu.edu>
2019-06-11 15:07:40 -0400

    Add the function to calculate Tr[(R_tot)^2]

--- shabanipy/quantum_hall/wal/universal/trajectory_parameter.py
+++ shabanipy/quantum_hall/wal/universal/trajectory_parameter.py
@@ -1,10 +1,12 @@
 from trajectories import generate_trajectory, identify_trajectory
 from numba import njit
 import numpy as np
+from numpy import *
 import pandas as pd
-from math import sqrt, pow, acos
+from math import *
 
 def find_each_length(seed: int, n_scat: int) -> np.ndarray:
+    #Find the length of each segment
 
     tj1 = generate_trajectory(seed, n_scat)
     tj2 = pd.DataFrame(tj1, columns = ['x', 'y'])
@@ -24,6 +26,8 @@ def find_each_length(seed: int, n_scat: int) -> np.ndarray:
 
 
 def find_each_angle(seed: int, n_scat: int) -> np.ndarray:
+    #find the angle of each segment to the x-axis
+
     tj1 = generate_trajectory(seed, n_scat)
     tj2 = pd.DataFrame(tj1, columns = ['x', 'y'])
 
@@ -43,6 +47,7 @@ def find_each_angle(seed: int, n_scat: int) -> np.ndarray:
     return angle
 
 def find_total_length(seed: int, n_scat: int) -> float:
+    #find the sum of the length of all the segments
 
     tj1 = generate_trajectory(seed, n_scat)
     tj2 = pd.DataFrame(tj1, columns = ['x', 'y'])
@@ -63,6 +68,7 @@ def find_total_length(seed: int, n_scat: int) -> float:
 
 
 def find_each_length_x(seed: int, n_scat: int) -> np.ndarray:
+    #find the projection of each length in x-axis
 
     tj1 = generate_trajectory(seed, n_scat)
     tj2 = pd.DataFrame(tj1, columns = ['x', 'y'])
@@ -81,6 +87,8 @@ def find_each_length_x(seed: int, n_scat: int) -> np.ndarray:
     return length_x
 
 def find_each_length_y(seed: int, n_scat: int) -> np.ndarray:
+    #find the projection of each length in y-axis
+
 
     tj1 = generate_trajectory(seed, n_scat)
     tj2 = pd.DataFrame(tj1, columns = ['x', 'y'])
@@ -97,3 +105,18 @@ def find_each_length_y(seed: int, n_scat: int) -> np.ndarray:
     length_y.append(d)
 
     return length_y
+
+def find_trace(lx: np.ndarray, ly: np.ndarray, angle: np.ndarray) -> complex:
+    #calculate the Tr[(R_tot)^2]
+
+    A = array([[1, 0], [0, 1]], dtype = complex)
+
+    for i in range(0, len(lx)):
+        theta = angle[i]
+        l1 = lx[i] + ly[i]
+        l2 = lx[i] - ly[i]
+        a1 = array([[cos(theta/2), -1j * l2 * sin(theta/2)], [-1j * l1 * sin(theta/2), cos(theta/2)]], dtype = complex)
+        A = dot(a1, A)
+
+    T = trace(dot(A, A))
+    return T

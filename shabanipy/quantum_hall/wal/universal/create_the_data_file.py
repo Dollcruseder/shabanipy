7efdbb8975c04add0f19eaecda1d7dab0b93a474
Dollcrusader <sy2297@nyu.edu>
2019-06-12 23:00:03 -0400

    Simplify the code and fix some mistake

--- shabanipy/quantum_hall/wal/universal/create_the_data_file.py
+++ shabanipy/quantum_hall/wal/universal/create_the_data_file.py
@@ -25,14 +25,14 @@ for i in range(0, len(number)):
 #for i in range(n1 - 1, n2):
     g1 = f1.create_group(f"n={i + 1}")
 
-    tj1 = generate_trajectory(seed[i], n_scat[i])
-    tj2 = pd.DataFrame(tj1, columns = ['x', 'y'])
-    x = tj2["x"]
-    y = tj2["y"]
-    l = find_each_length(seed[i], n_scat[i])
-    angle = find_each_angle(seed[i], n_scat[i])
-    lx = find_each_length_x(seed[i], n_scat[i])
-    ly = find_each_length_y(seed[i], n_scat[i])
+    tj = generate_trajectory(seed[i], n_scat[i])
+    x = tj[0]
+    y = tj[1]
+    x[-1] = 0
+    y[-1] = 0
+
+    l = find_each_length(x, y)
+    angle = find_each_angle(x, y)
 
     dset = g1.create_dataset("x", (len(x),))
     dset[...] = x
@@ -43,20 +43,11 @@ for i in range(0, len(number)):
     dset = g1.create_dataset("l", (len(l),))
     dset[...] = l
 
-    dset = g1.create_dataset("l_x", (len(lx),))
-    dset[...] = lx
-
-    dset = g1.create_dataset("l_y", (len(ly),))
-    dset[...] = ly
-
     dset = g1.create_dataset("angle", (len(angle),))
     dset[...] = angle
-
-    T = find_trace(lx, ly, angle)
 
     g1.attrs["Length"] = L[i]
     g1.attrs["n_scat"] = n_scat[i]
     g1.attrs["seed"] = seed[i]
     g1.attrs["Surface"] = S[i]
     g1.attrs["cosj'"] = cosjf[i]
-    g1.attrs["trace"] = T

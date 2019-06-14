98a688d22fcb2aa5cac462fad050f7c9d2b242d8
Dollcrusader <sy2297@nyu.edu>
2019-06-14 16:38:59 -0400

    Small change

--- shabanipy/quantum_hall/wal/universal/trace.py
+++ shabanipy/quantum_hall/wal/universal/trace.py
@@ -2,10 +2,11 @@ import h5py
 from trajectory_parameter import find_trace
 from math import pi
 
-f1 = h5py.File("cal_data.hdf5", "r")
-f2 = h5py.File("trace_data.hdf5", "w")
-
 N_orbit = 147885
+f1 = h5py.File("cal_data.hdf5", "r")
+f2 = h5py.File(f"trace_data_{N_orbit}.hdf5", "w")
+
+
 step = 0.25 * pi
 
 for i in [0, 1, 2, 4, 8]:

8451a29f3ea1be999d3c2f22943dbe3d604bdf0d
Dollcrusader <sy2297@nyu.edu>
2019-06-26 15:28:01 -0400

    plot the data except the mismatching terms

--- shabanipy/quantum_hall/wal/universal/plot.py
+++ shabanipy/quantum_hall/wal/universal/plot.py
@@ -3,22 +3,26 @@ from magnetoconductivity import MC
 import numpy as np
 from math import pow, pi
 
-L_phi = 10
+L_phi = int(input("L_phi="))
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.set(xlim=[-2, 1], title=rf'$L_\phi = {L_phi}$',
-       ylabel=r'$\Delta\sigma / (2e^2 / h)$', xlabel='$log(B/B_t)$')
+ax.set(title=rf'$L_\phi = {L_phi}$',
+       ylabel=r'$\sigma / (2e^2 / h)$', xlabel='$B/B_t$',
+       xscale='log')
 n_s = np.arange(3, 5001)
 F = np.sum(1 / (n_s - 2))
+for N_orbit in [147818]:
+    for theta_R in [0, 1, 2, 4, 8]:
+        x = np.linspace(-2, 1, 301)
+        x1 = 10**x
 
-for theta_R in [0, 1, 2, 4, 8]:
-    x = np.linspace(-2, 1, 301)
-    y = []
+        y = np.empty(len(x))
 
-    for i in range(0, len(x)):
-        x1 = pow(10, x[i])
-        y.append(-F * MC(x1, L_phi, theta_R) / (2 * pi))
+        for i in range(0, len(x)):
+            y[i] = -F * MC(x1[i], L_phi, theta_R, N_orbit) / (2 * pi)
 
-    ax.plot(x, y, label = rf"$\theta_R = {theta_R*0.25}\pi$")
+        ax.plot(x1, y, label = rf"$\theta_R = {theta_R*0.25}\pi$")
+
+
 plt.legend(loc='upper left')
 plt.show()
--- shabanipy/quantum_hall/wal/universal/trace.py
+++ shabanipy/quantum_hall/wal/universal/trace.py
@@ -2,8 +2,8 @@ import h5py
 from trajectory_parameter import find_trace
 from math import pi
 
-N_orbit = 147885
-f1 = h5py.File("cal_data.hdf5", "r")
+N_orbit = 147818
+f1 = h5py.File("data_for_trace_cal.hdf5", "r")
 f2 = h5py.File(f"trace_data_{N_orbit}.hdf5", "w")
 
 
@@ -14,7 +14,7 @@ for i in [0, 1, 2, 4, 8]:
     T = []
     print("i = ", i)
 
-    for n in range (1, N_orbit + 1):
+    for n in range (0, N_orbit):
         l = f1[f"n={n}"]["l"][:]
         angle = f1[f"n={n}"]["angle"][:]
         T_a = find_trace(l, angle, theta)

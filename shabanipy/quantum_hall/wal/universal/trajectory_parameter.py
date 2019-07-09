fe6b151facecab4f25481820adaed8ba2e01023b
Dollcrusader <sy2297@nyu.edu>
2019-07-09 17:21:27 -0400

    Delete useless function

--- shabanipy/quantum_hall/wal/universal/trajectory_parameter.py
+++ shabanipy/quantum_hall/wal/universal/trajectory_parameter.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2019 by ShabaniPy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the MIT license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""
+Compute some parameters in the trajectories
+"""
+
 from numba import njit
 import numpy as np
 from math import sqrt, atan2, cos, sin
@@ -39,41 +51,3 @@ def find_each_angle(x: np.ndarray, y: np.ndarray) -> np.ndarray:
 
     """
     return np.arctan2((y[1:] - y[:-1]), (x[1:] - x[:-1]))
-
-@njit
-def find_trace(l: np.ndarray, angle: np.ndarray, theta_R: float) -> float:
-    """Find the trace of the matrix R_tot^2
-
-    Parameters
-    ----------
-    l: np.ndarray
-        (n_scat) array, length of each segment
-    angle: np.ndarray
-        (n_scat) array, angle of each segment
-    theta_R: float
-        the spin rotation angle per the MFP length
-
-    Returns
-    -----------
-    trace: float
-        The trace of the matrix R_tot^2
-
-
-    """
-    rotations = np.empty((len(l), 2, 2), dtype=np.complex128)
-    theta = theta_R*l
-    c_theta = np.cos(0.5*theta)
-    s_theta = np.sin(0.5*theta)
-    c_phi = np.cos(angle)
-    s_phi = np.sin(angle)
-    rotations[:, 0, 0] = c_theta
-    rotations[:, 0, 1] =  (c_phi - 1j*s_phi)*s_theta
-    rotations[:, 1, 0] =  - (c_phi + 1j*s_phi)*s_theta
-    rotations[:, 1, 1] = c_theta
-
-    cw_rot = np.array([[1, 0], [0, 1]], dtype=np.complex128)
-    for i in range(0, len(l)):
-        cw_rot = rotations[i] @ cw_rot
-
-
-    return np.trace(cw_rot @ cw_rot).real

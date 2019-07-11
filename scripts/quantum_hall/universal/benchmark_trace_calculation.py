a0557e79b5b653c6fa69d0cb551561373913e175
MatthieuDartiailh <marul@laposte.net>
2019-07-11 09:17:57 -0400

    scripts: draft a trace calculation benchmarking script

---
+++ scripts/quantum_hall/universal/benchmark_trace_calculation.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright 2019 by ShabaniPy Authors, see AUTHORS for more details.
+#
+# Distributed under the terms of the MIT license.
+#
+# The full license is in the file LICENCE, distributed with this software.
+# -----------------------------------------------------------------------------
+"""Script to benchmark the calculation of the trace over the trajectories.
+
+"""
+
+# --- Parameters --------------------------------------------------------------
+
+#: Number of trajectories to consider.
+TRAJECTORY_NUMBER = 40000
+
+#: Parameters to use in the calculation: alpha, beta1, beta3, k, hvf
+PARAMETERS = ()
+
+# --- Imports -----------------------------------------------------------------
+

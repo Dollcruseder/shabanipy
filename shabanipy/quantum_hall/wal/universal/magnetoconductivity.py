f79a234567a94300a53dd563b0a5bc5b08eaf5c5
Dollcrusader <sy2297@nyu.edu>
2019-08-19 14:00:17 -0400

    change some code to do the fitting

--- shabanipy/quantum_hall/wal/universal/magnetoconductivity.py
+++ shabanipy/quantum_hall/wal/universal/magnetoconductivity.py
@@ -13,6 +13,7 @@ from math import cos, exp, pi
 
 import numpy as np
 from numba import njit, prange
+from shabanipy.quantum_hall.wal.universal.find_trace import compute_traces
 
 
 def MC(x: float, L_phi: float, T:np.ndarray, S: np.ndarray, L: np.ndarray, cosj: np.ndarray) -> float:
@@ -21,3 +22,15 @@ def MC(x: float, L_phi: float, T:np.ndarray, S: np.ndarray, L: np.ndarray, cosj:
     a = xj * np.cos(x * S)
 
     return np.sum(a) / len(T)
+
+"""
+def get_MC(field: np.ndarray, field_tr: float, L_phi: float, S: np.ndarray, L: np.ndarray, cosj: np.ndarray,
+           index: np.ndarray, l, c_phi: np.ndarray, c_3phi: np.ndarray, s_phi: np.ndarray, s_3phi: np.ndarray,
+           alpha: float, beta1: float, beta3: float, k: float, hvf: float, N_orbit: int) -> np.ndarray:
+    x = field / field_tr
+    T = compute_traces(index, l, c_phi, c_3phi, s_phi, s_3phi, alpha, beta1, beta3, k, hvf, N_orbit)
+    y = np.empty(len(x))
+    for i in range(0, len(x)):
+        y[i] = MC(x[i], L_phi, T, S, L, cosj)
+    return y
+"""

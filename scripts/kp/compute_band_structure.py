b9abd936bf0cb9264edcbe7a802e73a93a5b4ce3
MatthieuDartiailh <marul@laposte.net>
2020-01-08 17:10:44 -0500

    scripts: allow direct comparison for HgTe between old and new impl

--- scripts/kp/compute_band_structure.py
+++ scripts/kp/compute_band_structure.py
@@ -1,5 +1,5 @@
 #: Range (min, max, point_number) in which to vary the wave-vector (nm^-1)
-RANGE_K = (-0.8, 0.8, 401)
+RANGE_K = (-0.8, 0.8, 201)
 
 #: Orthogonal axis wave-vector
 FIXED_K = 0
@@ -19,27 +19,31 @@ from shabanipy.kp.parameters.materials import MATERIAL_PARAMETERS, make_alloy, l
 from shabanipy.kp.parameters.hamiltonian import HamiltonianParameters1D
 from shabanipy.kp import find_conduction_band
 from shabanipy.kp.hamiltonian_one_side_difference import build_hamiltonian
-# from shabanipy.kp.hamiltonian import build_hamiltonian
+# from shabanipy.kp.hamiltonian_one_side_difference_new import build_hamiltonian
+from shabanipy.kp.hamiltonian import build_hamiltonian as build_hamiltonian_old
 from shabanipy.kp.types import np_float
 import matplotlib.pyplot as plt
 import math
 
 
-def compute_spectrum(kx, ky, parameters, band_number):
+def compute_spectrum(kx, ky, parameters, band_number, old=False):
 
-    energies = np.empty((len(kx), parameters.site_number*band_number))
+    energies = np.empty((len(ky), parameters.site_number*band_number))
     # energies = np.empty((len(kx), 8))
 
-    for i in range(len(kx)):
-        h = build_hamiltonian(kx[i], ky, parameters, band_number)
+    for i in range(len(ky)):
+        if old:
+            h = build_hamiltonian_old(ky[i], -ky[i], parameters, band_number)
+        else:
+            h = build_hamiltonian(ky[i], -ky[i], parameters, band_number)
         # eigvalues = np.linalg.eigvalsh(h[:8,:8])
         eigvalues = np.linalg.eigvalsh(h)
         energies[i] = np.sort(eigvalues)
 
     return energies
 
-kx = np.linspace(*RANGE_K)
-ky = FIXED_K
+ky = np.linspace(*RANGE_K)
+kx = FIXED_K
 
 layer_1 = LayerParameters(5, ['InAs', 'GaAs'], [0.81, 0.19])
 
@@ -51,19 +55,19 @@ layer_4 = LayerParameters(20, ['InAs', 'AlAs'], [0.81, 0.19])
 
 layers = [layer_1, layer_2, layer_3, layer_4]
 
-# layer_1 = LayerParameters(4, ['hgte', 'cdte'], [0.32, 0.68])
-#
-# layer_2 = LayerParameters(8, ['hgte'], [1.0])
-#
-# layer_3 = LayerParameters(4, ['hgte', 'cdte'], [0.32, 0.68])
-#
-# layers = [layer_1, layer_2, layer_3]
+layer_1 = LayerParameters(6, ['hgte', 'cdte'], [0.32, 0.68])
+
+layer_2 = LayerParameters(4, ['hgte'], [1.0])
+
+layer_3 = LayerParameters(6, ['hgte', 'cdte'], [0.32, 0.68])
+
+layers = [layer_1, layer_2, layer_3]
 
 # layers = [LayerParameters(8, ['InAs'], [1.0])]
 
 
-substrate = load_substrate_parameters("InAs")
-# substrate = load_substrate_parameters("cdte")
+# substrate = load_substrate_parameters("InAs")
+substrate = load_substrate_parameters("cdte")
 
 
 interface = INTERFACE_THICKNESS
@@ -81,17 +85,15 @@ h_par = well.generate_hamiltonian_parameters(step)
 #                                 DiscretizedMaterialParameters1D(*parameters.T),)
 
 energies = compute_spectrum(kx, ky, h_par, BAND_NUMBER)
+energies_old = compute_spectrum(kx, ky, h_par, BAND_NUMBER, True)
 
-energies_k0 = energies[20]
-band_index = 8 * int(math.floor(np.argmin(np.abs(energies_k0))/8))
 fig, ax = plt.subplots(constrained_layout=True)
 ax.set(xlabel=r'$k_x$', ylabel='energy(eV)')
 
-# for i in range(16):
-#     ax.plot(kx, energies.T[band_index-i])
-#     ax.plot(kx, energies.T[band_index+i])
 for e in energies.T:
-    ax.plot(kx, e)
+    ax.plot(ky, e, 'b')
+for e in energies_old.T:
+    ax.plot(ky, e, 'r')
 
-#plt.legend()
+plt.ylim((-0.2, 0.4))
 plt.show()
